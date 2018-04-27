33000e71ad2a246c0c10d6b7936d07c9e938127d
Enric Balletbo i Serra <eballetbo@gmail.com>
2018-04-27 23:08:14 +0200

    Use X, Y, Z, A, B as axis of machine names.
    E0 and E1 is specific to the Ramps 1.4 board and we want to be more
    generic. So, use A and B axis to replace E0 and E1.
    
    Signed-off-by: Enric Balletbo i Serra <eballetbo@gmail.com>

--- Moveo.ino
+++ Moveo.ino
@@ -5,55 +5,55 @@
 #include <AccelStepper.h>
 
 // For RAMPS 1.4
-#define X0_STEP_PIN         54
-#define X0_DIR_PIN          55
-#define X0_ENABLE_PIN       38
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
 
-#define Y0_STEP_PIN         60
-#define Y0_DIR_PIN          61
-#define Y0_ENABLE_PIN       56
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
 
-#define Z0_STEP_PIN         46
-#define Z0_DIR_PIN          48
-#define Z0_ENABLE_PIN       62
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
 
-#define E0_STEP_PIN         26
-#define E0_DIR_PIN          28
-#define E0_ENABLE_PIN       24
+#define A_STEP_PIN         26 // E0_STEP_PIN
+#define A_DIR_PIN          28 // E0_DIR_PIN
+#define A_ENABLE_PIN       24 // E0_ENABLE_PIN
 
-#define E1_STEP_PIN         36
-#define E1_DIR_PIN          34
-#define E1_ENABLE_PIN       30
-    
+#define B_STEP_PIN         36 // E1_STEP_PIN
+#define B_DIR_PIN          34 // E1_DIR_PIN
+#define B_ENABLE_PIN       30 // E1_ENABLE_PIN
+
 // Define the steppers and the pins it will use
-AccelStepper StepperX0(AccelStepper::DRIVER, X0_STEP_PIN, X0_DIR_PIN);
-AccelStepper StepperY0(AccelStepper::DRIVER, Y0_STEP_PIN, Y0_DIR_PIN);
-AccelStepper StepperZ0(AccelStepper::DRIVER, Z0_STEP_PIN, Z0_DIR_PIN);
-AccelStepper StepperE0(AccelStepper::DRIVER, E0_STEP_PIN, E0_DIR_PIN);
-AccelStepper StepperE1(AccelStepper::DRIVER, E1_STEP_PIN, E1_DIR_PIN);
+AccelStepper StepperX(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN);
+AccelStepper StepperY(AccelStepper::DRIVER, Y_STEP_PIN, Y_DIR_PIN);
+AccelStepper StepperZ(AccelStepper::DRIVER, Z_STEP_PIN, Z_DIR_PIN);
+AccelStepper StepperA(AccelStepper::DRIVER, A_STEP_PIN, A_DIR_PIN);
+AccelStepper StepperB(AccelStepper::DRIVER, B_STEP_PIN, B_DIR_PIN);
 
 void setup()
 {  
-  // Configure stepper X0
-  StepperX0.setMaxSpeed(1000);
-  StepperX0.setSpeed(2 * 200);
-  StepperX0.setEnablePin(X0_ENABLE_PIN);
-  // Configure stepper Y0
-  StepperY0.setMaxSpeed(1000);
-  StepperY0.setSpeed(2 * 200);
-  StepperY0.setEnablePin(Y0_ENABLE_PIN);
-  // Configure stepper Z0
-  StepperZ0.setMaxSpeed(1000);
-  StepperZ0.setSpeed(2 * 200);
-  StepperZ0.setEnablePin(Z0_ENABLE_PIN);
-  // Configure stepper E0
-  StepperE0.setMaxSpeed(1000);
-  StepperE0.setSpeed(2 * 200);
-  StepperE0.setEnablePin(E0_ENABLE_PIN);
-  // Configure stepper E1
-  StepperE1.setMaxSpeed(1000);
-  StepperE1.setSpeed(2 * 200);
-  StepperE1.setEnablePin(E1_ENABLE_PIN);
+  // Configure stepper X
+  StepperX.setMaxSpeed(1000);
+  StepperX.setSpeed(2 * 200);
+  StepperX.setEnablePin(X_ENABLE_PIN);
+  // Configure stepper Y
+  StepperY.setMaxSpeed(1000);
+  StepperY.setSpeed(2 * 200);
+  StepperY.setEnablePin(Y_ENABLE_PIN);
+  // Configure stepper Z
+  StepperZ.setMaxSpeed(1000);
+  StepperZ.setSpeed(2 * 200);
+  StepperZ.setEnablePin(Z_ENABLE_PIN);
+  // Configure stepper A
+  StepperA.setMaxSpeed(1000);
+  StepperA.setSpeed(2 * 200);
+  StepperA.setEnablePin(A_ENABLE_PIN);
+  // Configure stepper B
+  StepperB.setMaxSpeed(1000);
+  StepperB.setSpeed(2 * 200);
+  StepperB.setEnablePin(B_ENABLE_PIN);
 
   Serial.begin(115200);
 }
@@ -74,7 +74,7 @@ void loop()
   long steps;
 
   // Change this variable to point to another stepper
-  stepper = &StepperX0;
+  stepper = &StepperX;
 
   Serial.setTimeout(2000); // 2 seconds
   if (Serial.available()) {
